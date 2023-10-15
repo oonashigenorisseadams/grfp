6aa3cc0d75b2a5e1b878bd187e5cf4da494e4dcd
oonashigenorisseadams <oonatoyou@gmail.com>
2023-10-14 17:37:43 -0700

    plot doneeeee

--- bedtools_analysis/concat_files.sh
+++ bedtools_analysis/concat_files.sh
@@ -3,6 +3,6 @@
 # This script concatenates all the lac bed files, run within all_bed
 
 for id in $(find . -name "*bed" | cut -f1 -d_ | sort | uniq); do
-  cat $(find . -wholename "${id}*") > ${id}_notab.bed
-  awk -F'\t' 'BEGIN {OFS = FS} {$7 = "seg"$2; print}' ${id}_notab.bed > ${id}.bed
+  cat $(find . -wholename "${id}*") > ${id}.bed
+  #awk -F'\t' 'BEGIN {OFS = FS} {$7 = "seg"$2; print}' ${id}_notab.bed > ${id}.bed
 done
